a031b01c4ecbbc449676ebf9e0d300cdc580c99e
cool208 <ajsalpatharakkal557@gmail.com>
2025-09-02 01:52:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -38,15 +38,16 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  const labels = ['Mammootty','Mohanlal','Dulquer salmaan','Ajsal','Zayed bin Sultan Al Nahyan','sheikh mohammed bin rashid al maktoum','Libi_maam',]
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`  https://minecraft12309.github.io/image/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
+
       return new faceapi.LabeledFaceDescriptors(label, descriptions)
     })
   )
